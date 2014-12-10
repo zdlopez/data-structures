3237b917a91a71ae53118fc2f63e1cae5a075d1f
zdlopez <zachary.d.lopez@gmail.com>
2014-12-10 15:19:52 -0800

    Add functional sharing for both

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,44 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  someInstance.first = 0;
+  extend(someInstance, queueMethods);
+  return someInstance;
+
 };
 
+var extend = function(to, from){
+  for(var key in from){
+    to[key] = from[key];
+  }
+};
+
+
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.storage[this.counter] = value;
+  this.counter++;
+};
+
+queueMethods.dequeue = function(){
+  if(this.counter>this.first){
+    var value = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+  }
+  return value;
+};
+
+queueMethods.size = function(){
+  return (this.counter-this.first);
+};
+
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  extend(someInstance, stackMethods);
+
+  return someInstance;
+};
+
+var extend = function(to, from){
+  for(var key in from){
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
+
+
+stackMethods.push = function(value){
+  this.counter++;
+  this.storage[this.counter] = value;
+};
+
+stackMethods.pop = function(){
+  if(this.counter > 0){
+    var value = this.storage[this.counter];
+    delete this.storage[this.counter];
+    this.counter--;
+    return value;
+  }
+};
+
+stackMethods.size = function(){
+  return this.counter;
+};
 
 
