9549351829e9ee4f861a25ccf30b10208a03cc65
zdlopez <zachary.d.lopez@gmail.com>
2014-12-10 14:41:11 -0800

    Add functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
+  //counter === 0 means stack is empty
 
   // Implement the methods below
   someInstance.push = function(value){
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function(){
+    if(counter > 0){
+      var value = storage[counter];
+      delete storage[counter];
+      counter--;
+      return value;
+    }
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
