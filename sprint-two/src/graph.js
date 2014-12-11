fb14bfb2457b3f540a2c58ffaf73fa3d21786740
zdlopez <zachary.d.lopez@gmail.com>
2014-12-11 10:38:39 -0800

    Initial - 3 passing on graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,21 +1,50 @@
 
 
-var Graph = function(){
+var Graph = function(node){
+  this.graph = {};
+  this.node = node;
+  this.edges = [];
 };
 
 Graph.prototype.addNode = function(node){
+  this.edges.push(new Graph(node));
 };
 
 Graph.prototype.contains = function(node){
+  var flag = false;
+  if(this.node === node){
+    flag = true;
+  }
+  for(var i = 0; i < this.edges.length; i++){
+    flag = flag || this.edges[i].contains(node);
+  }
+  return flag;
 };
 
 Graph.prototype.removeNode = function(node){
+  //var flag = false;
+  if(this.node === node){
+    delete this.node;
+  }
+  for(var i = 0; i < this.edges.length; i++){
+    this.edges[i].removeNode(node);
+  }
+  //return flag;
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var flag = false;
+  if(this.node === node){
+    flag = true;
+  }
+  for(var i = 0; i < this.edges.length; i++){
+    flag = flag || this.edges[i].contains(node);
+  }
+  return flag;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
